068a6a65798f24a0c5abd43ba7bbfea75c9ca5ba
c0a2405455 <165914564+c0a2405455@users.noreply.github.com>
2025-05-20 15:36:23 +0900

    共通基本機能

---
+++ game.py
@@ -0,0 +1,90 @@
+import os
+import random
+import sys
+import time
+import pygame as pg
+
+
+WIDTH = 650  # ゲームウィンドウの幅
+HEIGHT = 110  # ゲームウィンドウの高さ
+NUM_OF_BOMBS = 5  # 爆弾の個数
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Bird:
+    """
+    ゲームキャラクター（こうかとん）に関するクラス
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0),
+    }
+    img0 = pg.transform.rotozoom(pg.image.load("fig/campus.jpg"), 0, 0.9)
+    img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+    imgs = {  # 0度から反時計回りに定義
+        (+5, 0): img,  # 右
+        (+5, -5): pg.transform.rotozoom(img, 45, 0.9),  # 右上
+        (0, -5): pg.transform.rotozoom(img, 90, 0.9),  # 上
+        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9),  # 左上
+        (-5, 0): img0,  # 左
+        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9),  # 左下
+        (0, +5): pg.transform.rotozoom(img, -90, 0.9),  # 下
+        (+5, +5): pg.transform.rotozoom(img, -45, 0.9),  # 右下
+    }
+
+    def __init__(self, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数 xy：こうかとん画像の初期位置座標タプル
+        """
+        self.img = __class__.imgs[(+5, 0)]
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = xy
+
+    def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        screen.blit(self.img, self.rct)
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        self.rct.move_ip(sum_mv)
+        if check_bound(self.rct) != (True, True):
+            self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = __class__.imgs[tuple(sum_mv)]
+        screen.blit(self.img, self.rct)
+
+      
+def main():
+    pg.display.set_caption("たたかえ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))    
+    bg_img = pg.image.load("fig/campus.jpg")
+    bird = Bird((300, 200))
+    tmr = 0
+    while True:
+        
+        
+        pg.display.update()
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
